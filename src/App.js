84b4763aaf3593f6d10313bb332041934abb725c
rubenshibu <rubenshibu@gmail.com>
2020-03-27 01:00:46 +0530

    reinitialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Count, HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 import * as React from 'react';
-import './App.css';
+import './App';
 class App extends React.Component {
     constructor() {
         super(...arguments);
@@ -28,23 +28,7 @@ class App extends React.Component {
     }
     render() {
         return (<RichTextEditorComponent toolbarSettings={this.toolbarSettings} fontFamily={this.fontFamily} cssClass={this.cssClass}>
-        <p>The rich text editor component is WYSIWYG ("what you see is what you get") editor that provides the best user experience to create and update the content.
-          Users can format their content using standard toolbar commands.</p>
-        <p><b>Key features:</b></p>
-        <ul>
-          <li>
-            <p>Provides &lt;IFRAME&gt; and &lt;DIV&gt; modes</p>
-          </li>
-          <li>
-            <p>Capable of handling markdown editing.</p>
-          </li>
-          <li>
-            <p>Contains a modular library to load the necessary functionality on demand.</p>
-          </li>
-          <li>
-            <p>Provides a fully customizable toolbar.</p>
-          </li>
-        </ul>
+    
         <Inject services={[Toolbar, Count, Image, Link, HtmlEditor, QuickToolbar]}/>
       </RichTextEditorComponent>);
     }
